a346b2f1a3506863a4bcf6dfbe7c7a206ccd6dac
Joanderson <jhonny_040996@hotmail.com>
2023-05-11 21:02:32 -0300

    base

---
+++ src/components/Icon/types.tsx
@@ -0,0 +1,16 @@
+import { ReactComponent as Arow } from './svg/aroow.svg';
+
+
+
+const IconsTypes = {
+  arow : Arow,
+};
+
+export default IconsTypes;
+
+export type IconName =
+  | 'arow'
+
+
+
+
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import { Icon } from '@/components/Icon';
+
+export function Home(){
+  return(
+    <Icon name='arow' />
+  );
+}
